13be9e12c99bb5a5a061cdfa769af1c68a662444
bilbobagg1ns <mprasad@regis.edu>
2018-09-19 14:47:46 -0600

    Added Pagination behaviour to native query

--- src/main/java/com/bizapp/sample/model/data/repository/BusinessEntityRepository.java
+++ src/main/java/com/bizapp/sample/model/data/repository/BusinessEntityRepository.java
@@ -2,6 +2,7 @@ package com.bizapp.sample.model.data.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -26,6 +27,21 @@ public interface BusinessEntityRepository extends JpaRepository<BusinessEntity, 
 
 	@Query(value = "SELECT * FROM entity WHERE entity_nm LIKE %:entityName% LIMIT 10", nativeQuery = true)
 	List<BusinessEntity> findByPartialEntityNameNativeQuery(@Param("entityName") String entityName);
+
+	/*-
+	 * @See
+	 * https://stackoverflow.com/questions/38349930/spring-data-and-native-query-with-pagination
+	 * 
+	 * Observations:
+	 * 1. Default returned page has 20 items (size=20)
+	 * 2. To get a different number per page send size parameter:
+	 *   Ex: To return 40 items per page: 
+	 *    http://localhost:8080/api/businessEntities/search/findByPartialEntityNameNativeQueryWithPagination?entityName=Hope&size=40
+	 * 
+	 */
+	@Query(value = "SELECT * FROM entity WHERE entity_nm LIKE %:entityName%", countQuery = "SELECT count(*) FROM entity WHERE entity_nm LIKE %:entityName%", nativeQuery = true)
+	Page<BusinessEntity> findByPartialEntityNameNativeQueryWithPagination(@Param("entityName") String entityName,
+			Pageable pageable);
 
 	/* exact match */
 	List<BusinessEntity> findFirst20ByEntityName(@Param("entityName") String entityName, Pageable pageable);
